4ee7d9fa0185e47202d19015a2e1ca4877cbb65c
condac <burns.nu@gmail.com>
2022-02-08 21:16:19 +0100

    mfd lua

---
+++ plugins/xlua/scripts/mfd/mfd.lua
@@ -0,0 +1,5 @@
+-- Multi Function Display
+
+jas_io_fi_knapp_knappram = find_dataref("JAS/io/fi/knapp/knappram")
+jas_io_ti_knapp_knappram = find_dataref("JAS/io/ti/knapp/knappram")
+jas_io_mi_knapp_knappram = find_dataref("JAS/io/mi/knapp/knappram")
